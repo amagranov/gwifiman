2e2700aa3c331968781fae089b8db7e7f0f75c58
Andrey Magranov <amagranov@gmail.com>
2026-04-29 10:00:49 +0300

    update readme

--- install.sh
+++ install.sh
@@ -143,10 +143,15 @@ function refreshTimer() {
   const currentTime = new Date();
   const diffMs = wifiExpTime - currentTime;
   if (diffMs > 0) {
-   const totalSeconds = Math.floor(diffMs / 1000);
+   let totalSeconds = Math.floor(diffMs / 1000);
+   const days = Math.floor(totalSeconds / (24 * 60 * 60);
+   totalSeconds = (totalSeconds - days * 24 * 60 * 60)
+   const hours = Math.floor(totalSeconds / (60 * 60);
+   totalSeconds = (totalSeconds - hours * 60 * 60)
    const minutes = Math.floor(totalSeconds / 60);
    const seconds = totalSeconds % 60;
-   e.innerHTML = 'Осталось: ' + minutes + ":" + seconds.toString().padStart(2, '0');
+   let daysStr = days > 0 ? 
+   e.innerHTML = 'Осталось: ' + (days > 0) ? days + "д. " : "") + hours.toString().padStart(2, '0') + minutes + ":" + seconds.toString().padStart(2, '0');
    setTimeout(refreshTimer, 1000);
   } else {
    e.innerHTML = 'Истек срок действия<br>Дождитесь обновления';
